13a7394a0ba7e864f6d49d5f7e444b943ae5fab8
Pedro Mendes <pedrolopesme@gmail.com>
2017-06-03 17:34:50 -0300

    Improving back to main activity workflow from movie detail

--- app/src/main/java/com/pedrolopesme/android/cinepedia/activities/MovieDetailActivity.java
+++ app/src/main/java/com/pedrolopesme/android/cinepedia/activities/MovieDetailActivity.java
@@ -79,8 +79,7 @@ public class MovieDetailActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        Intent myIntent = new Intent(getApplicationContext(), MainActivity.class);
-        startActivityForResult(myIntent, 0);
+        finish();
         return true;
     }
 
